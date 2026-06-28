6815eb8842a5ac83e4c1c69541d12181032fe59c
juanmaaidar <juanmanuelaidar@gmail.com>
2026-06-27 22:49:32 -0300

    Remove obsolete tests and related files for account management functionality.

--- backend/cypress.config.ts
+++ backend/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   video: false,
-  fixturesFolder: 'src/test/javascript/cypress/fixtures',
+  fixturesFolder: false,
   screenshotsFolder: 'target/cypress/screenshots',
   downloadsFolder: 'target/cypress/downloads',
   videosFolder: 'target/cypress/videos',
@@ -16,12 +16,6 @@ export default defineConfig({
     jwtStorageName: 'jhi-authenticationToken',
   },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    async setupNodeEvents(on, config) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      return (await import('./src/test/javascript/cypress/plugins/index')).default(on, config);
-    },
     baseUrl: 'http://localhost:8081/',
     specPattern: 'src/test/javascript/cypress/e2e/**/*.cy.ts',
     supportFile: 'src/test/javascript/cypress/support/index.ts',
